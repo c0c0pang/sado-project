3ff1f66d4196305d73d58928ddb503280489adad
c0c0pang <coc1789@naver.com>
2023-10-08 21:42:15 +0900

    feat: recoil setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import DocButton from './button/DocButton';
 import Topbar from './nav/Topbar';
-
+import { RecoilRoot } from 'recoil';
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <Topbar />
       <DocButton />
-    </>
+    </RecoilRoot>
   );
 }
 
