e4a762655a6e7984a8ff8d25f4a6864faac78b52
c0c0pang <coc1789@naver.com>
2023-10-08 21:43:59 +0900

    feat: 상단 바 추가

---
+++ src/nav/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TopbarDiv, TopbarText } from './topbarStyle'
+
+function Topbar() {
+    return (
+        <TopbarDiv>
+            <TopbarText>사.도</TopbarText>
+        </TopbarDiv>
+    )
+}
+
+export default Topbar
