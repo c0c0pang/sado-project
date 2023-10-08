807bcdd9baef962161494844e3ad7f9289e46e23
c0c0pang <coc1789@naver.com>
2023-10-08 21:43:45 +0900

    feat: doc 카테고리에 쓰일 data 추가

---
+++ src/data/DocData.js
@@ -0,0 +1,18 @@
+export const selectList = [
+    {
+        name: '은행',
+        color: '#FFCA3A'
+    },
+    {
+        name: '구청',
+        color: '#3B5998'
+    },
+    {
+        name: '창업지원센터',
+        color: '#86E57F'
+    },
+    {
+        name: '투자',
+        color: '#CACFD2'
+    },
+]
