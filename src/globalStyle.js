ebbce96ceb8227c870e4f7f42702d944c5b45b64
c0c0pang <coc1789@naver.com>
2023-10-09 12:56:13 +0900

    feat: Gmarket 폰트 설정 및 A4 규격 컴포넌트 추가

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,5 +1,26 @@
 import { styled } from "styled-components";
-
 export const CafeText = styled.div`
     font-family: 'Cafe24';
+`
+export const GmarketLText = styled.div`
+    font-family: 'GmarketL';
+`
+export const GmarketMText = styled.div`
+    font-family: 'GmarketM';
+`
+export const GmarketBText = styled.div`
+    font-family: 'GmarketB';
+`
+
+export const A4 = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    background-color: white;
+    height: 297mm;
+    width: 210mm;
+    @page{
+        size: 210mm 297mm; /* A4 용지 크기 */
+        margin: 0;
+    }
 `
