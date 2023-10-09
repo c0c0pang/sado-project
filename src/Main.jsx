a7ff591bd22ada42761a6782aebad9feb8631a16
c0c0pang <coc1789@naver.com>
2023-10-09 13:09:02 +0900

    feat: 상단바와 버튼을 Main 컴포넌트로 묶음

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Topbar from './nav/Topbar'
+import DocButton from './button/DocButton'
+
+function Main() {
+    return (
+        <>
+            <Topbar />
+            <DocButton />
+        </>
+    )
+}
+
+export default Main
