a5494231cd311d7ff2ad70a6fa3736c4806a0901
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-11 01:05:03 +0900

    [Refactor] 합성컴포넌트 부분 수정

--- src/docFiles/pages/FirstPage.jsx
+++ src/docFiles/pages/FirstPage.jsx
@@ -9,60 +9,190 @@ function FirstPage(){
         <S.A4>
             <S.Container>
                 <Grid>
-                    <div style={{display: "flex"}}>
-                        <Grid.LeftDiv>
-                            회사명
+                    <div style={{display:"flex"}}>
+                        <Grid.LeftDiv percent={20}>
+                            <S.Wrapper>
+                                성 명
+                            </S.Wrapper>
                         </Grid.LeftDiv>
-                        <Grid.RightDiv>
+                        <Grid.RightDiv percent={80}>
                             <Grid.TextAreas/>
                         </Grid.RightDiv>
                     </div>
                     
-                    <div style={{display: "flex"}}>
-                        <Grid.LeftDiv>
-                            설 립 년 월 일
+                    <div style={{display:"flex"}}>
+                        <Grid.LeftDiv percent={20}>
+                            <S.Wrapper>
+                                주민등록번호
+                            </S.Wrapper>
                         </Grid.LeftDiv>
-                        <Grid.RightDiv>
+                        <Grid.RightDiv percent={80}>
+                            <Grid.TextAreas/>
+                        </Grid.RightDiv>
+                    </div>
+
+                    <div style={{display:"flex"}}>
+                        <Grid.LeftDiv percent={20}>
+                            <S.Wrapper>
+                                본 적
+                            </S.Wrapper>
+                        </Grid.LeftDiv>
+                        <Grid.RightDiv percent={80}>
                             <Grid.TextAreas/>
                         </Grid.RightDiv>
                     </div>
                     
-                    <div style={{display: "flex"}}>
-                        <Grid.LeftDiv>
-                            사업자등록번호
+                    <div style={{display:"flex"}}>
+                        <Grid.LeftDiv percent={20}>
+                            <S.Wrapper>
+                                현 주 소
+                            </S.Wrapper>
                         </Grid.LeftDiv>
-                        <Grid.RightDiv>
+                        <Grid.RightDiv percent={80}>
                             <Grid.TextAreas/>
                         </Grid.RightDiv>
                     </div>
-                    <div style={{display: "flex"}}>
-                        <Grid.LeftDiv>
-                            대표자
+
+                    
+                    <div style={{display:"flex"}}>
+                        <Grid.LeftDiv percent={20}>
+                            <Grid.LeftDiv percent={100}>
+                                <S.Wrapper>
+                                    학<br/><br/><br/>력
+                                </S.Wrapper>
+                            </Grid.LeftDiv>
+                            
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    기 간
+                                </S.Wrapper>
+                                <S.Wrapper>
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
                         </Grid.LeftDiv>
-                        <Grid.RightDiv>
-                            <Grid.TextAreas/>
-                        </Grid.RightDiv>
-                    </div>
-                    <div style={{display: "flex"}}>
-                        <Grid.LeftDiv>
-                            <Grid.Left1Right2>
-                                <div>소재지</div>
-                                <div>본사</div>
-                                <div style={{fontSize: "8px"}}>공장(농장)</div>
-                            </Grid.Left1Right2>
-                        </Grid.LeftDiv>
-                        <Grid.RightDiv>
-                            <Grid.TextAreas/>
-                            <Grid.TextAreas/>
+                        <Grid.RightDiv percent={80}>
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    학교명
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
+                            
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    전공분야
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
+                            
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    수학상태
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
                         </Grid.RightDiv>
                     </div>
                     
-                    <div style={{display: "flex"}}>
-                        <Grid.LeftDiv>
-                            대표자
+                    <div style={{display:"flex"}}>
+                        <Grid.LeftDiv percent={20}>
+                            <Grid.LeftDiv percent={100}>
+                                <S.Wrapper>
+                                    경<br/><br/><br/>력
+                                </S.Wrapper>
+                            </Grid.LeftDiv>
+                            
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    기 간
+                                </S.Wrapper>
+                                <S.Wrapper>
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
                         </Grid.LeftDiv>
-                        <Grid.RightDiv>
-                            <Grid.TextAreas/>
+                        <Grid.RightDiv percent={80}>
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    기 업 체 명
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
+                            
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    주 생 산 품 목
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
+                            
+                            <Grid.RightColDiv percent={100}>
+                                <S.Wrapper>
+                                    최종직위
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                                <S.Wrapper>    
+                                    <Grid.TextAreas/>
+                                </S.Wrapper>
+                            </Grid.RightColDiv>
                         </Grid.RightDiv>
                     </div>
                 </Grid>
@@ -82,7 +212,20 @@ const S = {
         margin: 0;
     }
     `,
-    
+    Wrapper: styled.div`
+        display: flex;  
+        min-height: 40px;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+    `,
+    LargeWrapper: styled.div`
+        display: flex;  
+        min-height: 80px;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+    `,
     Container: styled.div`
         margin: 50px;
         height: 80%;
